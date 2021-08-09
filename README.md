6d8e880622a7e758918423abbc637c224092351e
Zach Abney <zabney@headstorm.com>
2021-08-08 22:33:55 -0500

    Add support for setting multiple cookies at time

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # azure-aws-serverless-express
 
-Wrapper library to use aws-serverless-express with Azure Functions
+Wrapper library to use aws-serverless-express with Azure Functions.
+
+Note: This is a fork of [bigx333's library](https://github.com/bigx333/azure-aws-serverless-express) that adds support for setting multiple cookies at a time using the [context cookies object](https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-node?tabs=v2#response-object).
 
 ## Installation
 
@@ -12,7 +14,7 @@ npm install azure-aws-serverless-express --save
 
 ```javascript
 const express = require('express');
-const azureFunctionHandler = require('azure-aws-serverless-express');
+const azureFunctionHandler = require('@zachabney/azure-aws-serverless-express');
 
 const app = express();
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,53 @@
 import awsServerlessExpress from 'aws-serverless-express';
 import url from 'url';
 
+function getAzureCookies(headers) {
+  const cookies = Object.entries(headers)
+    .filter(([key, value]) => key.toLowerCase() === 'set-cookie')
+    .map(([key, value]) => {
+      const azureCookieObject = value.split(';')
+        .reduce((azureCookieObject, cookieProperty, index) => {
+          const [key, value] = cookieProperty.split('=')
+
+          const sanitizedKey = key.toLowerCase().trim()
+          const sanitizedValue = value && value.trim()
+
+          if (index === 0) {
+            azureCookieObject.name = sanitizedKey
+            azureCookieObject.value = sanitizedValue
+            return azureCookieObject
+          }
+
+          // https://github.com/Azure/azure-functions-nodejs-worker/blob/67941a159bf893b9459fd9a9bbdd691f8ba10838/types/public/Interfaces.d.ts#L120-L143
+          switch (sanitizedKey) {
+            case 'expires':
+              azureCookieObject.expires = new Date(sanitizedValue)
+              break
+            case 'secure':
+              azureCookieObject.secure = true
+              break
+            case 'httponly':
+              azureCookieObject.httpOnly = true
+              break
+            case 'samesite':
+              azureCookieObject.sameSite = sanitizedValue
+              break
+            case 'maxage':
+              azureCookieObject.maxAge = sanitizedValue
+              break
+            default:
+              azureCookieObject[sanitizedKey] = sanitizedValue
+          }
+
+          return azureCookieObject
+        }, {})
+
+        return azureCookieObject
+    })
+
+  return cookies
+}
+
 export default function azureFunctionHandler(app, binaryTypes) {
   binaryTypes = binaryTypes || ['*/*'];
 
@@ -21,7 +68,7 @@ export default function azureFunctionHandler(app, binaryTypes) {
     const awsContext = {
       succeed(awsResponse) {
         context.res.status = awsResponse.statusCode;
-        context.res.headers = {
+        const headers = {
           ...context.res.headers,
           ...awsResponse.headers
         };
@@ -29,7 +76,15 @@ export default function azureFunctionHandler(app, binaryTypes) {
           awsResponse.body,
           awsResponse.isBase64Encoded ? 'base64' : 'utf8'
         );
+
         context.res.isRaw = true;
+        context.res.cookies = getAzureCookies(headers)
+        context.res.headers = Object.entries(headers)
+          .filter(([key, value]) => key.toLowerCase() !== 'set-cookie')
+          .reduce((headers, [key, value]) => {
+            headers[key] = value
+            return headers
+          }, {})
 
         context.done();
       }
